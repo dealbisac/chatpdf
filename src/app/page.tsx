9735060b3f28bf86823153e59c95b9e4b8cecfec
dealbisac <chand.dipendra19@gmail.com>
2023-09-07 23:32:12 +0200

    Feature: started basic setup of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
 import Image from 'next/image'
+import { UserButton, auth } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  const isAuth = !!userId;
+
+
   return (
-    <div className='flex text-center'>
-      <h1>Welcome to Chat PDF</h1>
+    <div className='w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100'>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className='mr-3 text-5xl font-semibold'>
+              Welcome to Chat PDF
+            </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth &&
+              <Button>
+                Go to Chats
+              </Button>
+            }
+          </div>
+
+          <p className='max-w-xl mt-1 text-lg text-slate-600'>
+            Join millions of students, researchers, and professionals to instantly answer your questions and understand difficult concepts with ease and speed with AI.
+          </p>
+
+          <div className="w-full mt-4">
+            { isAuth ? (
+              <h1 className='text-2xl font-semibold'>
+                fileupload
+              </h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started!
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
